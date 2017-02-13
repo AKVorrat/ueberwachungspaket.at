15622d275784643049d7920efa9ed9236ddb7d16
PublicScam <publicscam@users.noreply.github.com>
2017-02-13 16:23:28 +0100

    Add sensitive config.

--- ueberwachungspaket/__init__.py
+++ ueberwachungspaket/__init__.py
@@ -3,6 +3,7 @@ from .representatives import Representatives
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
+app.config.from_pyfile("config_sensitive.py")
 reps = Representatives()
 
 if __name__ == "__main__":
--- ueberwachungspaket/config.py
+++ ueberwachungspaket/config.py
@@ -1,10 +1,5 @@
 import os
 
-DEBUG = False
+DEBUG = True
 
 PROJECT_NAME = "Kontaktiere Deine Abgeordneten"
-
-TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
-TWILIO_NUMBER = os.environ.get("TWILIO_NUMBER")
-TWIML_APPLICATION_SID = os.environ.get("TWIML_APPLICATION_SID")
