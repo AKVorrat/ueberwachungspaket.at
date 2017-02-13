e5bf215131551996c7b1501d44428041b92ef3a7
PublicScam <publicscam@users.noreply.github.com>
2017-02-13 23:16:57 +0100

    Add smooth scroll, link FAQ, add representative name.

--- ueberwachungspaket/static/js/site.js
+++ ueberwachungspaket/static/js/site.js
@@ -1,3 +1,22 @@
+// smooth scroll
+
+$(document).ready(function () {
+	$("a[href*='#']:not([href='#'])").click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')  || location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 800);
+				return false;
+			}
+		}
+	});
+});
+
+// activate search filter
+
 $(document).ready(function() {
 	if($("#representatives").length) {
 		$("#parties :checkbox").change(search);
