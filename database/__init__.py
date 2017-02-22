53e3c9be5de120cdde87aa9a51d8f232834b981e
PublicScam <publicscam@users.noreply.github.com>
2017-02-22 18:03:13 +0100

    Fix MySQL timeout.

--- database/__init__.py
+++ database/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from config import *
 
 db_path = "mysql+pymysql://{}:{}@{}/{}".format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
-engine = create_engine(db_path, convert_unicode=True)
+engine = create_engine(db_path, convert_unicode=True, pool_recycle=3600)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          bind=engine))
 
