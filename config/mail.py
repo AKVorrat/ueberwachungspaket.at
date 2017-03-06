ef34e7e99da4a3bbd1a6e7951db29ae9312bbab7
Thomas Lohninger <thomas@lohninger.org>
2017-03-06 19:39:08 +0100

    update mail tempaltes

--- config/mail.py
+++ config/mail.py
@@ -5,15 +5,15 @@ MAIL_DISCLAIMER = "Diese E-Mail wird übermittelt im Namen von: {name_user}, {ma
 MAIL_REPRESENTATIVE = """
 {salutation} {name_rep}, 
 
-das geplante Überwachungspaket im neuen Regierungsprogram vom 30. Jänner 2017 erfüllt mich mit großer Besorgnis. Die vorgeschlagenen Maßnahmen laufen auf eine Totalüberwachung aller Menschen in Österreich hinaus. Egal ob man mit dem Auto fährt, öffentliche Verkehrsmittel verwendet, sich auf öffentlichen Plätzen bewegt, oder zu den 4,5 Millionen Menschen mit anonymen SIM Karten gehört: Bald soll die gesamte Bevölkerung auf Schritt und Tritt überwacht werden. Das will ich nicht! Ich habe ein Recht auf Privatsphäre.
+das geplante Überwachungspaket im neuen Regierungsprogram vom 30. Jänner 2017 erfüllt mich mit großer Besorgnis. Die vorgeschlagenen Maßnahmen laufen auf eine Totalüberwachung aller Menschen in Österreich hinaus. Egal ob man mit dem Auto fährt, öffentliche Verkehrsmittel verwendet, sich auf öffentlichen Plätzen bewegt, oder zu den 4,5 Millionen Menschen mit anonymen SIM Karten zählt: Bald soll die gesamte Bevölkerung auf Schritt und Tritt überwacht werden. Das will ich nicht! Ich habe ein Recht auf Privatsphäre.
 
-Für keine der vorgestellten Maßnahmen wurden Belege geliefert, dass sie tatsächlich einen Sicherheitsgewinn bringen. Sicher ist nur, dass meine Freiheit damit eingeschränkt wird. 
+Für keine der vorgestellten Maßnahmen wurden Belege geliefert, dass sie tatsächlich einen Sicherheitsgewinn bringen. Sicher ist nur, dass mit ihnen meine Freiheit eingeschränkt wird. 
 
 Videoüberwachung kann Terroristen sogar motivieren, da damit Bilder von ihnen und ihren Gräueltaten in die Öffentlichkeit gelangen. Die Registrierung von Prepaid-SIM-Karten wurde bereits in vielen Ländern geprüft und abgelehnt; sogar die EU Innenkommissarin hat sich dagegen ausgesprochen. 
 
 Wieso wollen Sie meine reale Freiheit einem subjektiven Sicherheitsgefühl opfern? 
 
-Es ist die Aufgabe des Staates die Bevölkerung zu schützen. Dazu gehört auch die Wahrung der Grundrechte. Bitte sprechen Sie sich Sie gegen das Überwachungspaket aus! Verhindern Sie die Einrichtung eines lückenlosen Überwachungssystems! Setzen Sie sich für Grund- und Freiheitsrechte ein!
+Es ist die Aufgabe des Staates, die Bevölkerung zu schützen. Dazu gehört auch die Wahrung der Grundrechte. Bitte sprechen Sie sich Sie gegen das Überwachungspaket aus! Verhindern Sie die Einrichtung eines lückenlosen Überwachungssystems! Setzen Sie sich für Grund- und Freiheitsrechte ein!
 
 Hochachtungsvoll
 {name_user}
@@ -22,26 +22,36 @@ Hochachtungsvoll
 MAIL_WELCOME = """
 Liebe/r {name_user}, 
 
-wir danken Ihnen herzlich, dass sie sich für Ihr Grundrecht auf Privatsphäre und Datenschutz stark gemacht haben. Wir würden uns sehr freuen, wenn Sie mithelfen, dass möglichst viele Menschen vom geplanten Überwachungspaket erfahren. Wir alle sind davon betroffen und gemeinsam können wir etwas für unsere Freiheit tun.
+wir danken Ihnen herzlich, dass Sie sich für Ihr Grundrecht auf Privatsphäre und Datenschutz stark gemacht haben. Wir würden uns sehr freuen, wenn Sie mithelfen, dass möglichst viele Menschen vom geplanten Überwachungspaket erfahren. Wir alle sind davon betroffen und gemeinsam können wir etwas für unsere Freiheit tun.
 
-Erzählen Sie ihren Freunden von Ihrem Engagement: 
-<link auf FB post wie https://www.facebook.com/global2000/posts/10154996051913832>
-<link auf Tweet> 
+1))) Erzählen Sie ihren Freunden von Ihrem Engagement: 
 
-oder senden Sie eine E-Mail an fünf Freunde. Hier ist ein Textvorschlag dafür: 
-"<siehe unten "E-Mail zum Weiterleiten an Freunde">" 
 
-Gehen Sie den nächsten Schritt. Rufen Sie Ihre Abgeordneten an! 
-Die effektivste Art Politikern ins Gewissen zu reden ist, direkt mit ihnen am Telefon zu sprechen.
+2))) Senden Sie eine E-Mail an fünf Freunde. Hier ist ein Textvorschlag dafür: 
+"
+Hallo, 
 
+ich habe gerade meinen Abgeordneten wegen des geplanten Überwachungspakets kontaktiert. Die Regierung will uns alle mit lückenloser Überwachung unter Generalverdacht stellen. 
+
+Bitte schau dir www.überwachungspaket.at an und werde selbst für deine Rechte aktiv.
+
+Hilf mit, unsere Privatsphäre zu retten!
+
+Liebe Grüße,
+" 
+
+3))) Gehen Sie den nächsten Schritt! Rufen Sie Ihre Abgeordneten an! 
+Die effektivste Art, Politikern ins Gewissen zu reden ist, direkt mit ihnen am Telefon zu sprechen.
 Mit unserer Hotline geht das ganz einfach. Rufen Sie 
-0720 230 002
+0720 205088
 an und folgen Sie dem Sprachmenü.
+Mit einer Anruferinnerung leisten Sie einen wiederkehrenden Beitrag, die Überwachungspläne der Bundesregierung zu verhindern!  
 
-Mit einer Anruferinnerung leisten Sie einen regelmäßigen Beitrag, die Überwachungspläne der Bundesregierung zu verhindern!
 
 Zivilgesellschaft wirkt! 
-Unser Verein epicenter.works kämpft für den Erhalt von Grund- und Freiheitsrechten im Internet. Wir streiten vor Gericht, im Parlament und auf der Straße. Wir arbeiten unabhängig von Parteien und Unternehmen. Damit wir weiterhin schlagkräftig bleiben, sind wir auf Hilfe aus der Zivilgesellschaft angewiesen. Sie können uns als Fördermitglied oder mit einer Spende unterstützen: spenden.epicenter.works.
+Unser Verein epicenter.works kämpft für den Erhalt von Grund- und Freiheitsrechten im Internet. Wir streiten vor Gericht, im Parlament und auf der Straße. Wir arbeiten unabhängig von Parteien und Unternehmen. Damit wir weiterhin schlagkräftig bleiben, sind wir auf Hilfe aus der Zivilgesellschaft angewiesen. 
+Sie können uns als Fördermitglied oder mit einer Spende unterstützen: spenden.epicenter.works.
+
 
 Liebe Grüße, 
 
@@ -51,11 +61,12 @@ Ihr Team von epicenter.works
 MAIL_VALIDATE = """
 Liebe/r {name_user},
 
-vielen Dank für Ihren Einsatz für unsere Grundrechte. Damit Ihre E-Mail über www.überwachungspaket.at auch abgeschickt werden kann, müssen Sie auf den unten stehenden Link folgen:
+vielen Dank für Ihren Einsatz für unsere Grundrechte! Damit Ihre E-Mail über www.überwachungspaket.at auch abgeschickt werden kann, müssen Sie diesem Link folgen:
 {url}
 
 Falls Sie nicht wissen, wieso sie diese Nachricht erhalten haben, bitte ignorieren Sie diese Nachricht.
 
 Liebe Grüße,
+
 Ihr Team von epicenter.works
 """
