568976e98ce63a2f06e15b84c33d2c38ae86d2f1
PublicScam <publicscam@users.noreply.github.com>
2017-09-05 00:15:43 +0200

    Use blueprints, explicitly close connections, add connection ping.

--- ueberwachungspaket/views/act.py
+++ ueberwachungspaket/views/act.py
@@ -1,178 +1,20 @@
-# -*- coding: utf-8 -*-
-from random import choice, shuffle
-from datetime import datetime, date, timedelta
-from json import load
+from random import choice
+from datetime import datetime, timedelta
 from re import match
-from flask import render_template, abort, request, url_for, flash, redirect, jsonify, send_from_directory
-from sqlalchemy import func, and_
+from flask import (Blueprint, render_template, abort, request, url_for,
+                   flash, redirect)
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
-from config import *
-from config.main import *
-from config.mail import *
-from database.models import Representatives, Reminder, Mail, Sender, Opinion, ConsultationSender
-from . import app, db_session
-from .decorators import twilio_request
-import math
+from config import DEBUG, FEEDBACK_NUMBER, TWILIO_NUMBERS
+from database.models import (Representatives, Reminder, Mail, Sender)
+from database import db_session
+from ueberwachungspaket.decorators import twilio_request
 
-page_size = 35
 reps = Representatives()
+mod = Blueprint("act", __name__, url_prefix="/act")
 
-@app.route("/")
-def root():
-    with open("ueberwachungspaket/data/quotes.json", "r") as json_file:
-        quotes = load(json_file)
-    shuffle(quotes)
-    consultation_count = db_session.query(func.count(ConsultationSender.date_validated)).one()[0]
-    consultation_max = math.ceil(consultation_count / 10000.0) * 10000
-    consultation_percent = 100.0*consultation_count/consultation_max if consultation_count > 0 else 0
-    opinions = db_session.query(Opinion).count()
-    return render_template(
-        "index.html",
-        quotes=quotes,
-        opinion_count=opinions,
-        consultation_progress_max=consultation_max,
-        consultation_progress_count=consultation_count,
-        consultation_progress_count_percent=consultation_percent
-    )
 
-@app.route("/politiker/")
-def representatives():
-    reps_random = reps.representatives + reps.government
-    shuffle(reps_random)
-    return render_template(
-        "representatives.html",
-        reps=reps_random
-    )
-
-@app.route("/p/<prettyname>/")
-def representative(prettyname):
-    rep = reps.get_representative_by_name(prettyname)
-    if rep:
-        return render_template(
-            "representative.html",
-            rep=rep,
-            twilio_number=TWILIO_NUMBERS[0]
-        )
-    else:
-        abort(404)
-
-@app.route("/konsultation/")
-def consultation():
-    with open("ueberwachungspaket/data/quotes.json", "r") as json_file:
-        quotes = load(json_file)
-    shuffle(quotes)
-    query = db_session.query(Opinion).order_by(Opinion.originality.desc(), Opinion.date.desc())
-    opinions_count = query.count()
-    opinions = query.limit(page_size).all()
-
-    return render_template(
-        "consultation.html",
-        quotes=quotes,
-        opinions=opinions,
-        opinions_count=opinions_count
-    )
-
-@app.route("/konsultation/load")
-def consultation_load():
-    page_index = request.args.get("pageIndex", 0, type=int)
-    sort_key = request.args.get("sortKey")
-    filter_origin = request.args.get("filterOrigin")
-    filter_topic = request.args.get("filterTopic")
-    filter_name = request.args.get("filterName")
-
-    query = db_session.query(Opinion)
-
-    if sort_key:
-        if sort_key == "name":
-            query = query.order_by(Opinion.name, Opinion.originality.desc())
-        if sort_key == "-name":
-            query = query.order_by(Opinion.name.desc(), Opinion.originality.desc())
-        elif sort_key == "date":
-            query = query.order_by(Opinion.date, Opinion.originality.desc())
-        elif sort_key == "-date":
-            query = query.order_by(Opinion.date.desc(), Opinion.originality.desc())
-        elif sort_key == "-originality":
-            query = query.order_by(Opinion.originality, Opinion.date.desc())
-        else:
-            query = query.order_by(Opinion.originality.desc(), Opinion.date.desc())
-
-    if filter_topic:
-        if filter_topic == "bundestrojaner":
-            query = query.filter_by(addresses_bundestrojaner=True)
-        elif filter_topic == "netzsperren":
-            query = query.filter_by(addresses_netzsperren=True)
-        elif filter_topic == "vds-video":
-            query = query.filter_by(addresses_vds_video=True)
-        elif filter_topic == "ueberwachung-strassen":
-            query = query.filter_by(addresses_ueberwachung_strassen=True)
-        elif filter_topic == "vds-quickfreeze":
-            query = query.filter_by(addresses_vds_quickfreeze=True)
-        elif filter_topic == "anonyme-simkarten":
-            query = query.filter_by(addresses_anonyme_simkarten=True)
-        elif filter_topic == "imsi-catcher":
-            query = query.filter_by(addresses_imsi_catcher=True)
-        elif filter_topic == "lauschangriff-auto":
-            query = query.filter_by(addresses_lauschangriff_auto=True)
-
-    if filter_origin:
-        if filter_origin == "bmi":
-            query = query.filter(and_(Opinion.link_bmi_pdf.isnot(None), Opinion.link_bmi_pdf != ""))
-        elif filter_origin == "bmj":
-            query = query.filter(and_(Opinion.link_bmj_pdf.isnot(None), Opinion.link_bmj_pdf != ""))
-
-    if filter_name:
-        query = query.filter(Opinion.name.ilike("%{}%".format(filter_name)))
-
-    opinions_count = query.count()
-    opinions = query.slice(page_index * page_size, (page_index + 1) * page_size).all()
-    opinions = [opinion.serialize() for opinion in opinions]
-    return jsonify(opinions=opinions, count=opinions_count)
-
-@app.route("/konsultation/stats")
-def consultation_stats():
-    query = db_session.query(Opinion)
-    stats = {
-        "addressesBundestrojaner": query.filter_by(addresses_bundestrojaner=True).count(),
-        "addressesNetzsperren": query.filter_by(addresses_netzsperren=True).count(),
-        "addressesVdsVideo": query.filter_by(addresses_vds_video=True).count(),
-        "addressesUeberwachungStrassen": query.filter_by(addresses_ueberwachung_strassen=True).count(),
-        "addressesVdsQuickfreeze": query.filter_by(addresses_vds_quickfreeze=True).count(),
-        "addressesAnonymeSimkarten": query.filter_by(addresses_anonyme_simkarten=True).count(),
-        "addressesImsiCatcher": query.filter_by(addresses_imsi_catcher=True).count(),
-        "addressesLauschangriffAuto": query.filter_by(addresses_lauschangriff_auto=True).count()
-    }
-    return jsonify(stats=stats)
-
-@app.route("/konsultation/showpdf/bmi/<int:fid>")
-def showpdf_bmi(fid):
-    resp = send_from_directory(PDF_FOLDER, str(fid) + "_bmi.pdf")
-    resp.headers["Content-Disposition"] = "inline; filename=" + str(fid) + "_bmi.pdf"
-    return resp
-
-@app.route("/konsultation/showpdf/bmj/<int:fid>")
-def showpdf_bmj(fid):
-    resp = send_from_directory(PDF_FOLDER, str(fid) + "_bmj.pdf")
-    resp.headers["Content-Disposition"] = "inline; filename=" + str(fid) + "_bmj.pdf"
-    return resp
-
-@app.route("/weitersagen/")
-def share():
-    return render_template("share.html")
-
-@app.route("/unterstützer/")
-def supporters():
-    return render_template("supporters.html")
-
-@app.route("/faq/")
-def faq():
-    return render_template("faq.html")
-
-@app.route("/datenschutz/")
-def privacy():
-    return render_template("privacy.html")
-
-@app.route("/act/mail/", methods=["POST"])
+@mod.route("/mail/", methods=["POST"])
 def mail():
     id = request.form.get("id")
     rep = reps.get_representative_by_id(id)
@@ -182,7 +24,7 @@ def mail():
     newsletter = True if request.form.get("newsletter") == "yes" else False
 
     if not all([rep, firstname, lastname, mail_user]) or not rep.contact.mail or id == "00000" or not match(r"[^@]+@[^@]+\.[^@]+", mail_user):
-        abort(400) # bad request
+        abort(400)  # bad request
 
     name_user = firstname + " " + lastname
 
@@ -229,7 +71,8 @@ def mail():
 
     return redirect(url_for("representative", prettyname=rep.name.prettyname, _anchor="email-senden"))
 
-@app.route("/act/validate/<hash>", methods=["GET"])
+
+@mod.route("/validate/<hash>", methods=["GET"])
 def validate(hash):
     try:
         sender = db_session.query(Sender).filter_by(hash=hash).one()
@@ -251,13 +94,15 @@ def validate(hash):
 
     return render_template("validate.html")
 
-@app.route("/act/call/", methods=["POST"])
+
+@mod.route("/call/", methods=["POST"])
 @twilio_request
 def call(resp):
     resp.play(url_for("static", filename="audio/intro.wav"))
     resp.redirect(url_for("gather_menu"))
 
-@app.route("/act/callback/", methods=["POST"])
+
+@mod.route("/callback/", methods=["POST"])
 @twilio_request
 def callback(resp):
     direction = request.values.get("Direction")
@@ -277,7 +122,8 @@ def callback(resp):
     except NoResultFound:
         pass
 
-@app.route("/act/gather-menu/", methods=["POST"])
+
+@mod.route("/gather-menu/", methods=["POST"])
 @twilio_request
 def gather_menu(resp):
     number = request.values.get("From")
@@ -292,7 +138,8 @@ def gather_menu(resp):
     with resp.gather(numDigits=1, action=url_for("handle_menu")) as g:
         g.play(url_for("static", filename="audio/gather_menu.wav"), loop=4)
 
-@app.route("/act/handle-menu/", methods=["POST"])
+
+@mod.route("/handle-menu/", methods=["POST"])
 @twilio_request
 def handle_menu(resp):
     digits_pressed = request.values.get("Digits", 0, type=int)
@@ -311,13 +158,15 @@ def handle_menu(resp):
         resp.play(url_for("static", filename="audio/invalid.wav"))
         resp.redirect(url_for("gather_menu"))
 
-@app.route("/act/gather-reminder-time/", methods=["POST"])
+
+@mod.route("/gather-reminder-time/", methods=["POST"])
 @twilio_request
 def gather_reminder_time(resp):
     with resp.gather(numDigits=2, action=url_for("handle_reminder_time")) as g:
         g.play(url_for("static", filename="audio/gather_reminder_time.wav"), loop=4)
 
-@app.route("/act/handle-reminder-time/", methods=["POST"])
+
+@mod.route("/handle-reminder-time/", methods=["POST"])
 @twilio_request
 def handle_reminder_time(resp):
     digits_pressed = request.values.get("Digits", 0, type=int)
@@ -341,13 +190,15 @@ def handle_reminder_time(resp):
         resp.play(url_for("static", filename="audio/handle_reminder_time_invalid.wav"))
         resp.redirect(url_for("gather_reminder_time"))
 
-@app.route("/act/gather-representative/", methods=["POST"])
+
+@mod.route("/gather-representative/", methods=["POST"])
 @twilio_request
 def gather_representative(resp):
     with resp.gather(numDigits=5, action=url_for("handle_representative")) as g:
         g.play(url_for("static", filename="audio/gather_representative.wav"), loop=4)
 
-@app.route("/act/handle-representative/", methods=["POST"])
+
+@mod.route("/handle-representative/", methods=["POST"])
 @twilio_request
 def handle_representative(resp):
     digits_pressed = request.values.get("Digits", "00000", type=str)
@@ -363,7 +214,7 @@ def handle_representative(resp):
         resp.play(url_for("static", filename="audio/representative/" + rep.name.prettyname + ".wav"))
         resp.play(url_for("static", filename="audio/handle_representative_c.wav"))
 
-        if app.debug:
+        if DEBUG:
             resp.dial(FEEDBACK_NUMBER, timeLimit=60, callerId=choice(TWILIO_NUMBERS))
         else:
             resp.dial(rep.contact.phone, timeLimit=3600, callerId=choice(TWILIO_NUMBERS))
@@ -374,7 +225,8 @@ def handle_representative(resp):
         resp.play(url_for("static", filename="audio/handle_representative_invalid.wav"))
         resp.redirect(url_for("gather_representative"))
 
-@app.route("/act/handle-reminder-unsubscribe/", methods=["POST"])
+
+@mod.route("/handle-reminder-unsubscribe/", methods=["POST"])
 @twilio_request
 def handle_reminder_unsubscribe(resp):
     direction = request.values.get("Direction")
@@ -389,19 +241,22 @@ def handle_reminder_unsubscribe(resp):
     db_session.commit()
     resp.play(url_for("static", filename="audio/handle_reminder_unsubscribe.wav"))
 
-@app.route("/act/gather-reminder-call/", methods=["POST"])
+
+@mod.route("/gather-reminder-call/", methods=["POST"])
 @twilio_request
 def gather_reminder_call(resp):
     resp.play(url_for("static", filename="audio/gather_reminder_call.wav"))
     resp.redirect(url_for("gather_reminder_menu"))
 
-@app.route("/act/gather-reminder-menu/", methods=["POST"])
+
+@mod.route("/gather-reminder-menu/", methods=["POST"])
 @twilio_request
 def gather_reminder_menu(resp):
     with resp.gather(numDigits=1, action=url_for("handle_reminder_menu")) as g:
         g.play(url_for("static", filename="audio/gather_reminder_menu.wav"), loop=4)
 
-@app.route("/act/handle-reminder-menu/", methods=["POST"])
+
+@mod.route("/handle-reminder-menu/", methods=["POST"])
 @twilio_request
 def handle_reminder_menu(resp):
     digits_pressed = request.values.get("Digits", 0, type=int)
@@ -417,7 +272,7 @@ def handle_reminder_menu(resp):
         resp.play(url_for("static", filename="audio/representative/" + rep.name.prettyname + ".wav"))
         resp.play(url_for("static", filename="audio/handle_representative_c.wav"))
 
-        if app.debug:
+        if DEBUG:
             resp.dial(FEEDBACK_NUMBER, timeLimit=60, callerId=choice(TWILIO_NUMBERS))
         else:
             resp.dial(rep.contact.phone, timeLimit=900, callerId=choice(TWILIO_NUMBERS))
@@ -425,7 +280,7 @@ def handle_reminder_menu(resp):
         resp.play(url_for("static", filename="audio/adieu.wav"))
 
     elif digits_pressed == 2:
-        pass # hang up
+        pass  # hang up
     elif digits_pressed == 3:
         resp.redirect(url_for("reminder_info_tape"))
     elif digits_pressed == 4:
@@ -436,7 +291,8 @@ def handle_reminder_menu(resp):
         resp.play(url_for("static", filename="audio/invalid.wav"))
         resp.redirect(url_for("gather_reminder_menu"))
 
-@app.route("/act/info_tape/", methods=["POST"])
+
+@mod.route("/info_tape/", methods=["POST"])
 @twilio_request
 def info_tape(resp):
     with resp.gather(numDigits=1, action=url_for("gather_menu")) as g:
@@ -444,14 +300,11 @@ def info_tape(resp):
 
     resp.redirect(url_for("gather_menu"))
 
-@app.route("/act/reminder_info_tape/", methods=["POST"])
+
+@mod.route("/reminder_info_tape/", methods=["POST"])
 @twilio_request
 def reminder_info_tape(resp):
     with resp.gather(numDigits=1, action=url_for("gather_reminder_menu")) as g:
         g.play(url_for("static", filename="audio/info_tape.wav"))
 
     resp.redirect(url_for("gather_reminder_menu"))
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
