b451e284075256d66c771deca033f69646183bbf
Benedikt Gollatz <ben@differentialschokolade.org>
2019-05-27 10:49:38 +0200

    Revert "Add popup with link to IstKicklSchonWeg.at"
    This reverts commit 367f8bf4fc6c34894eda745f6b601de535760c7e.

--- ueberwachungspaket/static/js/site.js
+++ ueberwachungspaket/static/js/site.js
@@ -511,11 +511,6 @@ $(document).ready(function() {
         setSortKey(sortKey);
     });
 
-    $(".kickl-popup > button").click(function (e) {
-        e.preventDefault();
-        $(".kickl-popup").remove();
-    });
-
     $("body").on("click", ".commentOpener", function () {
         var commentId = $(this).data("comment");
         var comment = $("#comment" + commentId);
